bae281638dc4bc5b939b7e4460d53dc21122a50e
Harbs <harbs@in-tools.com>
2020-01-05 15:08:42 +0200

    Added new CSS files

---
+++ SpectrumBrowser/combine.sh
@@ -0,0 +1,18 @@
+(
+  cd src/assets/css/components
+  (
+    for d in */ ; do
+      echo "$d"
+      cd "$d"
+      rm -f -r colorStops
+      rm -f -r dist
+      rm -f index-lg.css
+      rm -f index-vars.css
+      rm -f package.json
+      rm -f vars.css
+      cat *.css */*.css > combined.css
+      cd ..
+    done
+
+  )
+)
